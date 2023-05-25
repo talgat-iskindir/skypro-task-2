e12360d11d28c6c1e577e3f63fe9a35b238fe75b
Talgat <talgat.iskindir3@gmail.com>
2023-05-25 07:41:56 +0200

    Add Resume Views

---
+++ resumes/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from resumes.views import ResumesApiView
+
+urlpatterns = [
+    path("resumes/<int:resume_id>/", ResumesApiView.as_view()),
+]
--- resumes/views.py
+++ resumes/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from typing import Optional
 
-# Create your views here.
+from django.http import Http404, JsonResponse
+from rest_framework import status
+from rest_framework.views import APIView
+
+from resumes.dtos import ResumeDto
+from resumes.models import Resume
+
+
+class ResumesApiView(APIView):
+    def get(self, request, resume_id):
+        resume: Optional[Resume] = Resume.objects.filter(id=resume_id).first()
+        if not resume:
+            raise Http404(f"Resume with {id} not found")
+
+        resume_dto = ResumeDto.from_orm(resume)
+
+        print(resume_dto.dict())
+
+        return JsonResponse(data=resume_dto.dict(), status=status.HTTP_200_OK)
+
+    def patch(self, request, resume_id):
+        resume_dto = ResumeDto(**request.data)
+        Resume.objects.filter(id=resume_id).update(**resume_dto.dict(exclude_none=True))
+
+        resume = Resume.objects.filter(id=resume_id).first()
+        if not resume:
+            raise Http404(f"Resume with {id} not found")
+
+        resume_dto = ResumeDto.from_orm(resume)
+
+        return JsonResponse(data=resume_dto.dict(), status=status.HTTP_202_ACCEPTED)
