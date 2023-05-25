2286486ab2f74a4525212d5cc2356cf01921472c
Talgat <talgat.iskindir3@gmail.com>
2023-05-25 08:33:15 +0200

    Fix views

--- resumes/views.py
+++ resumes/views.py
@@ -1,7 +1,8 @@
 from typing import Optional
 
-from django.http import Http404, JsonResponse
+from django.http import JsonResponse
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView
 
 from resumes.dtos import ResumeDto
@@ -9,25 +10,38 @@ from resumes.models import Resume
 
 
 class ResumesApiView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     def get(self, request, resume_id):
         resume: Optional[Resume] = Resume.objects.filter(id=resume_id).first()
         if not resume:
-            raise Http404(f"Resume with {id} not found")
+            return JsonResponse(
+                data={"error": f"Resume with ID {resume_id} not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
 
         resume_dto = ResumeDto.from_orm(resume)
-
-        print(resume_dto.dict())
 
         return JsonResponse(data=resume_dto.dict(), status=status.HTTP_200_OK)
 
     def patch(self, request, resume_id):
+        resume = Resume.objects.filter(id=resume_id).first()
+        if not resume:
+            return JsonResponse(
+                data={"error": f"Resume with ID {resume_id} not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        if not resume.owner or request.user.id != resume.owner.id:
+            return JsonResponse(
+                data={"error": f"Permission denied for resume with ID {resume_id}"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
         resume_dto = ResumeDto(**request.data)
         Resume.objects.filter(id=resume_id).update(**resume_dto.dict(exclude_none=True))
 
-        resume = Resume.objects.filter(id=resume_id).first()
-        if not resume:
-            raise Http404(f"Resume with {id} not found")
-
+        resume.refresh_from_db()
         resume_dto = ResumeDto.from_orm(resume)
 
         return JsonResponse(data=resume_dto.dict(), status=status.HTTP_202_ACCEPTED)
