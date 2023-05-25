9b095a39a9c9d8f53f3a01f535b7493c26f47c7b
Talgat <talgat.iskindir3@gmail.com>
2023-05-25 06:10:55 +0200

    Create models

--- resumes/models.py
+++ resumes/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from resumes.constants import RESUME_STATUSES, RESUME_CLOSE_STATUS, GRADES
+
+
+class Resume(models.Model):
+    status = models.CharField(max_length=32, choices=RESUME_STATUSES, default=RESUME_CLOSE_STATUS)
+    grade = models.CharField(max_length=32, choices=GRADES)
+    specialty = models.TextField()
+    salary = models.IntegerField()
+    education = models.TextField()
+    experience = models.TextField()
+    portfolio = models.TextField()
+    title = models.CharField(max_length=64)
+    phone = models.CharField(max_length=32)
+    email = models.EmailField()
+
+
+class ResumeOwner(models.Model):
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
